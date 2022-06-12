09d8dd801deb629c1477ec044e07ac15eef1d2e4
Vijay Choudhary <scriptyvijay@gmail.com>
2022-06-12 22:53:48 +0530

    added check feature

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Check from "./Check";
 
 const SearchBox = ({ searchfield, searchChange }) => {
 	return (
 		<div className="pa2">
 			<input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="Search Robots" onChange={searchChange} />
+			<Check />
 		</div>
 	);
 };
